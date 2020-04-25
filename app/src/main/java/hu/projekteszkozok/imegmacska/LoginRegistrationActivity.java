561452bc40022ffac9ecce277ae83deb24dfca82
Kormi9722 <kormi9716@gmail.com>
2020-04-25 03:23:52 +0200

    Login / Registration Screen Front End Done

---
+++ app/src/main/java/hu/projekteszkozok/imegmacska/LoginRegistrationActivity.java
@@ -0,0 +1,15 @@
+package hu.projekteszkozok.imegmacska;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginRegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_registration);
+    }
+
+}
